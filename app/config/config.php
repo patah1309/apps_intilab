cf298efdff73182160f9350c00d665cf2a8407dc
patah1309 <abdulpatah@intilab.com>
2023-06-26 12:48:11 +0700

    Patah - 2023-06-26

--- app/config/config.php
+++ app/config/config.php
@@ -1,14 +1,14 @@
 <?php
 $path = 'http://';
 if (isset($_SERVER['HTTPS']))$path = 'https://';
-$link = 'https://apps.intilab.com/eng/backend/public/';
+$link = 'http://10.88.0.139/eng/backend/public/';
 $url = $path . $_SERVER['SERVER_NAME'].':8080/public';
 $url_api = $link.'default/api';
 $url_foto = $link.'dokumentasi/';
 define('base_url', $url);
 define('base_api', $url_api);
 define('base_foto', $url_foto);
-// define('DB_HOST', 'localhost');
+// define('DB_HOST', 'localhost'); 
 // define('DB_USER', 'root');
 // define('DB_PASS', 'skyhwk12');
 // define('DB_NAME', 'gps');
